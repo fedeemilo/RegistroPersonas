7304de531ee3c7dd7daed16dcfb4b77a5fcd2545
fedeemilo <fedeemilo91@gmail.com>
2020-06-12 20:24:36 -0300

    fix db

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	HOST: process.env.DB_HOST,
+	HOST: process.env.DB_HOST || '127.0.0.1',
 	USER: process.env.DB_USER,
 	PASSWORD: process.env.DB_PASSWORD,
 	DB: process.env.DB_DATABASE,
