78b5a759b1bed190e0ef33b52d5812a28a455b00
fedeemilo <fedeemilo91@gmail.com>
2020-06-13 08:27:13 -0300

    add footer

--- src/public/assets/js/script-utils.js
+++ src/public/assets/js/script-utils.js
@@ -4,14 +4,16 @@
 
 const showTableButton = document.querySelector('#show-table');
 const showFormButton = document.querySelector('#show-form');
+const closeAlertButton = document.querySelector('.close');
 const personsTable = document.querySelector('#table-persons');
 const cardPersonsForm = document.querySelector('#card-persons-form');
+const alertDiv = document.querySelector('.alert-dismissible');
 
 document.addEventListener('DOMContentLoaded', () => {
 	/* On click toggle show/hide table  */
 	showTableButton.addEventListener('click', () => {
 		// Toggle show/hide table
-		personsTable.classList.toggle('d-table-if');
+		personsTable.classList.toggle('d-none');
 
 		// Change table button's text
 		console.log(showTableButton.innerHTML.trim());
@@ -23,11 +25,17 @@ document.addEventListener('DOMContentLoaded', () => {
 	/* On click toggle show/hide form */
 	showFormButton.addEventListener('click', () => {
 		// Toggle show/hide form
-		cardPersonsForm.classList.toggle('d-form-if');
+		cardPersonsForm.classList.toggle('d-none');
 
 		// Change form button's text
 		showFormButton.innerHTML.trim() === 'Ocultar Formulario'
 			? (showFormButton.innerHTML = 'Mostrar Formulario')
 			: (showFormButton.innerHTML = 'Ocultar Formulario');
+	});
+
+	/* Close alert */
+	closeAlertButton.addEventListener('click', () => {
+
+		alertDiv.style.display = 'none';
 	});
 });
